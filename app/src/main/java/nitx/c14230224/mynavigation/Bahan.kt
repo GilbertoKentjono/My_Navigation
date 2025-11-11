bc104b72b3cb742a269eb91ced6d8a7e581a73a2
GIlberto <c14230224@john.petra.ac.id>
2025-11-11 18:26:41 +0700

    mengubah halaman input dan icon cart

--- app/src/main/java/nitx/c14230224/mynavigation/Bahan.kt
+++ app/src/main/java/nitx/c14230224/mynavigation/Bahan.kt
@@ -1,6 +1,7 @@
 package nitx.c14230224.mynavigation
-
+import java.io.Serializable
 data class Bahan(
     var nama: String,
-    var kategori: String
-)
+    var kategori: String,
+    var gambarUrl: String
+) : Serializable
